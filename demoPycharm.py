479a57c39f80b25fc9add32899cb88145cf68ba4
Mee2020 <stablecourse@yahoo.com>
2020-04-19 18:51:36 -0500

    Two lines added, has next line too

--- demoPycharm.py
+++ demoPycharm.py
@@ -1,1 +1,4 @@
 print("Using Pycharm and GitHub for collaborative programming")
+# print these two statements below in separate lines
+print("No computer application is written by a single programmer", '\n')
+print("It's always a team effort")
